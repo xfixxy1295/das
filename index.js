b988a6095ef6ad8a9f838fd7ebf56a374eb86cae
xfixxy1295 <101675961+xfixxy1295@users.noreply.github.com>
2023-02-21 00:09:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443;
 app.use('/upload', express.static('upload'))
 // sendFile will go here
 app.get('/', function(req, res) {
